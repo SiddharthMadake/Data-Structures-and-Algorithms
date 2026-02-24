063d92d73d89357684aa1eaede8b5766013a314d
SiddharthMadake <siddharthmadake9@gmail.com>
2026-02-24 19:56:55 +0530

    remove duplicate at time complexity O(n)

---
+++ Array/Foundation/remove_duplicate.py
@@ -0,0 +1,11 @@
+#sorted array
+a=[1,3,4,5,3,4,1,2,4]
+a.sort()
+
+def remove(a):
+    b=[a[0]]
+    for i in range(1,len(a)):
+        if a[i]!=a[i-1]:
+            b.append(a[i])
+    return b
+print(remove(a))
