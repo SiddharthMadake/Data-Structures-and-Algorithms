d8a259594d9d33dd29db91b2da84cb98a0aeee0b
SiddharthMadake <siddharthmadake9@gmail.com>
2026-09-02 23:38:50 +0530

    bubble sort solved in time complexxity O(n2)

---
+++ Serching and Sorting/bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(a):
+    n=len(a)
+    for i in range(n):
+        for j in range(n-i-1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+    return a
+print(bubble_sort([3,6,2,8,5]))
